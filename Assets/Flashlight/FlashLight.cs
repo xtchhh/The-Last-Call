4b950e40dbaa56aea4a648c84d06ccf08808d1f9
xtchhh <chappellxander77@gmail.com>
2025-10-31 10:53:18 -0400

    Imported scripts and textures

---
+++ Assets/Flashlight/FlashLight.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashLight : MonoBehaviour
+{
+    public Animator flashLightAnim;
+    public bool IsOn { get; private set; } = true; // Default to on, change as needed
+
+    void Update()
+    {
+        // Example toggle logic (replace with your actual toggle input)
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            IsOn = !IsOn;
+            // Optionally trigger animation or light component here
+        }
+
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
+        {
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                flashLightAnim.ResetTrigger("Walk");
+                flashLightAnim.SetTrigger("Sprint");
+            }
+            else
+            {
+                flashLightAnim.ResetTrigger("Sprint");
+                flashLightAnim.SetTrigger("Walk");
+            }
+        }
+    }
+}
